8e421176458ce42cf018d6374361f3e06fd84cf0
santhoshkumar <santhoshkumar.ssk3222@gmail.com>
2019-02-20 10:43:42 +0530

    code

---
+++ Zilker/FirstModule/Controller/Page/HelloWorld.php
@@ -0,0 +1,36 @@
+<?php
+ /**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Zilker\FirstModule\Controller\Page;
+class HelloWorld extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\Controller\Result\JsonFactory
+     */
+    protected $resultJsonFactory;
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory)
+{
+       $this->resultJsonFactory = $resultJsonFactory;
+       parent::__construct($context);
+}
+    /**
+     * HelloWorld  page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+       $result = $this->resultJsonFactory->create();
+       $data = ['message' => 'Hello world!'];
+
+return $result->setData($data);
+} }
+?>
---
+++ Zilker/FirstModule/Controller/Page/Index.php
@@ -0,0 +1,32 @@
+<?php
+ /**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Zilker\FirstModule\Controller\Page;
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    protected $pageFactory;
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $pageFactory
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\View\Result\PageFactory $pageFactory)
+       {
+              $this->pageFactory = $pageFactory;
+		return parent::__construct($context);
+       }
+    /**
+     * Index  page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+       return $this->pageFactory->create();
+    } 
+}
+?>
